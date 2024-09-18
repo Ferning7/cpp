fc041f1d60eb2d3082b82e7aa6ff1fcdabc43ea8
Matheus Fernandes <matheusfffcorrea@gmail.com>
2024-09-18 14:57:05 -0300

    DoWhile

---
+++ doWhile/at01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    int i = 0;
+    
+    
+    cout << "Digite um numero inteiro positivo: ";
+    cin >> num;
+    
+    do{ 
+       cout << i << " ";
+        i += 2; 
+    } while (i <= num);
+    
+    
+    return 0;
+}
